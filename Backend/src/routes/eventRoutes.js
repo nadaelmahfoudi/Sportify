75a53751b0d8b8ce1d0c19361af551d1254c513e
Nada Elmahfoudi <elmahfoudinada17@gmail.com>
2024-11-27 10:39:11 +0100

    SPOR-5 Add abackend logic to update event with protected route

--- Backend/src/routes/eventRoutes.js
+++ Backend/src/routes/eventRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { createEvent } = require('../controllers/eventController');
+const { createEvent, updateEvent } = require('../controllers/eventController');
 const authenticate = require('../middlewares/authenticate');
 
 // Route to create an event
 router.post('/create', authenticate, createEvent);
+router.put('/:eventId', authenticate, updateEvent);
 module.exports = router;
