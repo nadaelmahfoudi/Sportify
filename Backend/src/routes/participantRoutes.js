121bf147b919bb518349584da272515b56c593e4
Nada Elmahfoudi <elmahfoudinada17@gmail.com>
2024-11-29 12:56:06 +0100

    SPOR-9 Delete Participant backend and frontend files

--- Backend/src/routes/participantRoutes.js
+++ Backend/src/routes/participantRoutes.js
@@ -2,10 +2,12 @@
 const express = require('express');
 const router = express.Router();
 const authenticate = require('../middlewares/authenticate');
-const { createParticipant, updateParticipant, getParticipants } = require('../controllers/participantController');
+const { createParticipant, updateParticipant, getParticipants, deleteParticipant } = require('../controllers/participantController');
 
 router.post('/add', authenticate, createParticipant);
 router.put('/:participantId', authenticate,  updateParticipant);
+router.delete('/:participantId', authenticate, deleteParticipant);
+
 router.get('/participants', getParticipants);
 
 module.exports = router;
